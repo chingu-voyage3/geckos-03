48707d20f21b2348c360f7e9c7e7e641d119f6a0
Michael Dimitras <dimitrasprojects@gmail.com>
2017-12-28 14:54:53 -0800

    Started database

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -247,6 +247,12 @@ function appendMessage(msg, name) {
 	chat.innerHTML += chatHTML(msg, name);
 }
 
+function loadCanvas(drawing) {
+
+	console.log(typeof(drawing));
+
+}
+
 /*
  * socket.io part
  */
@@ -261,6 +267,10 @@ socket.on('update users', userList =>{
 
 socket.on('msg', (msg, name) => {
 	appendMessage(msg, name);
+})
+
+socket.on('load canvas', drawing =>{
+	loadCanvas(drawing);
 })
 
 /*
